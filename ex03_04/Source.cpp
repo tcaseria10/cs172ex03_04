553e18737a1d9e0b1e101836c24fde26dfeff7ef
tcaseria10 <tcaseria19@LAB-SCI-304-16>
2017-03-07 00:46:41 -0800

    Finished

--- ex03_04/Source.cpp
+++ ex03_04/Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>	
+#include <string>
+#include <algorithm>
 using namespace std;
 
 // Tyler Caseria
@@ -6,12 +8,25 @@ using namespace std;
 // Hw 03 : Exercise 04
 // 23 February 2017
 
+string sort(string& s) {
+	string sss = s;
+	std::sort(sss.begin(), sss.end());
+	cout << "Your sorted string is: " << sss << "." << endl;
+	return sss;
+}
+
+
+
 int main() {
+	string ss;
+	cout << "Enter a string of characters:";
+	cin >> ss;
+	cout << endl;
+	sort(ss);
 
-
-
-
-
+	int one;
+	cout << "Press 1, then press enter to exit." << endl;
+	cin >> one;
 
 	return 0;
 }
